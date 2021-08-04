9e13903bc14014e9f9605ef972beba0b16a91344
lucasdepaula@dcc.ufmg.br <lucasdepaula@dcc.ufmg.br>
2021-08-04 14:29:53 -0300

    create init page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <p>Hello World!</p>
+    <div className="container">
+      <p className="title">Monografia - Lucas de Paula Silva</p>
+      <p className="subtitle">Teste de ferramentas analíticas compatíveis com a LGPD (Lei Geral de Proteção de Dados)</p>
     </div>
   );
 }
